e805b29761a456eaf8b8b520f6d3a01b330f0b86
Eddie Lopez <iameddielopez@gmail.com>
2020-07-14 17:38:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,13 +10,12 @@ def add_element_to_end_of_array(array,element)
   array << element
 end
 
-
 def add_element_to_start_of_array(array,element)
   array.unshift(element) 
 end
 
 def remove_element_from_end_of_array(array)
-  array = remove_element_from_end_of_array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
