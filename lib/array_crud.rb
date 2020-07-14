b2802e91bb1e5fce7ecc0c123ee584be7a0c51d6
Eddie Lopez <iameddielopez@gmail.com>
2020-07-14 15:50:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array[array,element]
   add_element_to_end_of_array << "element2"
 end
 
-def add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array[array, element]
   add_element_to_start_of_array.unshift("people")
 end
 
