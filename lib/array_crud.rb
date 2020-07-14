a198cbf90810c33eb1505cc4a87f93711117a0c9
Eddie Lopez <iameddielopez@gmail.com>
2020-07-14 16:28:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,30 +11,30 @@ def add_element_to_end_of_array([ ],element)
   add_element_to_end_of_array << "another_element"
 end
 
-def add_element_to_start_of_array[array,element]
+def add_element_to_start_of_array(array,element)
   add_element_to_start_of_array.unshift("people")
 end
 
-def remove_element_from_end_of_array[array]
+def remove_element_from_end_of_array(array)
   array = remove_element_from_end_of_array.pop
 end
 
-def remove_element_from_start_of_array[array]
+def remove_element_from_start_of_array(array)
   array = remove_element_from_start_of_array.shift
 end
 
-def retrieve_element_from_index[array,index_number]
+def retrieve_element_from_index(array,index_number)
   retrieve_element_from_index[0]
 end
 
-def retrieve_first_element_from_array[array]
+def retrieve_first_element_from_array(array)
   retrieve_first_element_from_array[0]
 end
 
-def retrieve_last_element_from_array[array]
+def retrieve_last_element_from_array(array)
   retrieve_last_element_from_array[-1]
 end
 
-def update_element_from_index[array,index_number,element]
+def update_element_from_index(array,index_number,element)
   update_element_from_index[1]= "new_index_number"
 end
